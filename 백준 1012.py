adadfba4d1036403bcac102364ca49db679e7553
kto0903 <kto0903@naver.com>
2023-01-08 19:54:51 +0900

    finish

--- 백준 1012.py
+++ 백준 1012.py
@@ -1,17 +1,12 @@
 from collections import deque
 import sys
-
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-
 a = int(input())
-
-
 def bfs(graph, a, b):
     q = deque()
     q.append((a, b))
     graph[a][b] = 0
-
     while q:
         x, y = q.popleft()
         for i in range(4):
@@ -23,17 +18,13 @@ def bfs(graph, a, b):
                 graph[nx][ny] = 0
                 q.append((nx, ny))
     return
-
-
 for i in range(a):
     result = 0
     n, m, k = map(int, sys.stdin.readline().split())
     graph = [[0]*m for _ in range(n)]
-
     for j in range(k):
         x, y = map(int, sys.stdin.readline().split())
         graph[x][y] = 1
-
     for a in range(n):
         for b in range(m):
             if graph[a][b] == 1:
