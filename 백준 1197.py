e3fd9bc4f91e603b757c23664acdde5b084a167f
kto0903 <kto0903@naver.com>
2023-01-28 18:46:26 +0900

    finish

---
+++ 백준 1197.py
@@ -0,0 +1,27 @@
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+edges = []
+result = 0
+for i in range(1, v + 1):
+    parent[i] = i
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+edges.sort()
+for edge in edges:
+    cost, a, b = edge
+    if find(a) != find(b):
+        union(a, b)
+        result += cost
+print(result)
